c4a2725986bb379f50a207008095714cc9c3da74
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-07-20 15:40:33 +0300

    sass structure, some styles and btn component

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import './lib/lib';
 
-console.log($('.findme').fadeOut(5000));
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
+});
+
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
+});
+
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
